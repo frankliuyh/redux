c17cf88b48d8ab2a58da1f34001ff1f595632242
frankliuyh <frankliuyh@qq.com>
2018-07-07 14:19:59 +0800

    connect react to redux

---
+++ src/store/reducer.js
@@ -0,0 +1,27 @@
+const intialState = {
+	counter: 0
+};
+
+const reducer = (state = intialState, action) => {
+	if (action.type === 'INCREMENT') {
+		return {
+			counter: state.counter + 1
+		};
+	}
+	if (action.type === 'DECREMENT') {
+		return {
+			counter: state.counter - 1
+		};
+	}if (action.type === 'ADD') {
+		return {
+			counter: state.counter + 5
+		};
+	}if (action.type === 'SUBTRACT') {
+		return {
+			counter: state.counter - 5
+		};
+	}
+	return state;
+};
+
+export default reducer;
